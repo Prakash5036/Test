5cd837c6505cef510ed2fd2818e25b0ce5b6463e
Prakash <Prakash5036@gmail.com>
2022-12-09 11:29:42 +0530

    New updated code

--- src/org/employee/Employee.java
+++ src/org/employee/Employee.java
@@ -13,6 +13,11 @@ public class Employee {
 		
 	}
 	
+	public void empPhone()
+	{
+		System.out.println("9941476036");
+	}
+	
 	public static void main(String[] args) {
 		
 		Employee a=new Employee();
